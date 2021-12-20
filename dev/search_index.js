29fb91313297f16481339ca16ee645d55c82aab3
Documenter.jl <documenter@juliadocs.github.io>
2021-12-20 13:23:03 +0000

    build based on 7eb3d22

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = InterSpikeSpectra","category":"page"},{"location":"#InterSpikeSpectra","page":"Home","title":"InterSpikeSpectra","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for InterSpikeSpectra.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [InterSpikeSpectra]","category":"page"}]
+}
