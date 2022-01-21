e435e80600d64001fecf4148eac4ff1d5d3eeb1c
K.H. Kraemer <hkraemer@pik-potsdam.de>
2022-01-21 20:23:48 +0100

    first try to reduce number of basis functions, does not work yet

--- src/basis_functions.jl
+++ src/basis_functions.jl
@@ -29,7 +29,35 @@ function inter_spike_spectrum(s::Vector{T}; ρ_thres::Real = 0.99, tol::Real=1e-
     N = length(s)
     Θ = generate_basis_functions(N)'
 
+    display("Size of Θ is: $(size(sparse(Θ)))")
+
     return compute_spectrum_according_to_threshold(s, sparse(Θ), ρ_thres, tol, max_iter, verbose)
+end
+
+"""
+    hallo
+"""
+function inter_spike_spectrum2(s::Vector{T}; ρ_thres::Real = 0.99, tol::Real=1e-3, max_iter::Integer=15, verbose::Bool=true) where {T}
+    @assert 0.8 <= ρ_thres <= 1 "Optional input `ρ_thres` must be a value in the interval [0.8, 1]"
+    @assert 1e-5 <= tol <= 1 "Optional input `tol` must be a value in the interval [1e-5, 1]"
+    @assert 1 < max_iter <= 20 "Optional input `max_iter` must be an integer in the interval (1, 20]."
+
+    # normalization
+    s = (s.-mean(s)) ./ std(s)
+    s .-= minimum(s)
+    s ./= maximum(s)
+
+    N = length(s)
+
+    non_unique_idx  = non_unique_lines(N)
+    Θ = generate_basis_functions(N)
+    size_full = size(Θ,1)
+    full_idx = collect(1:size(Θ,1))
+    setdiff!(full_idx, non_unique_idx)
+
+    display("Size of Θ is: $(size(sparse(view(Θ,full_idx,:)')))")
+
+    return compute_spectrum_according_to_threshold2(s, sparse(view(Θ,full_idx,:)'), ρ_thres, tol, max_iter, full_idx, size_full, verbose)
 end
 
 """
@@ -91,6 +119,61 @@ function compute_spectrum_according_to_threshold(s::Vector, Θ::SparseMatrixCSC,
     end
 end
 
+function compute_spectrum_according_to_threshold2(s::Vector, Θ::SparseMatrixCSC, ρ_thres::Real, tol::Real, max_iter::Integer, unique_idx::Vector, size_full::Int, verbose::Bool)
+    abs_tol = 1e-6
+    # initial Lambda-step for estimating an upper bound for λ
+    lambda_step = 0.5
+    lambda_max, lambda_min, y_act, ρ_act = find_lambda_max(s, Θ, lambda_step, ρ_thres)
+    actual_lambda = 0
+    # bisection search
+    for i = 1:max_iter
+        # check whether max iterations or tolerance-level reached
+        if i == max_iter
+            if ρ_act > 1 - abs_tol
+                if verbose
+                    println("Algorithm stopped due to maximum number of λ's were tried without convergence to the specified `ρ_thres`. Perfect deomposition achieved.")
+                end
+                spectrum_i = pool_frequencies2(y_act, length(s), unique_idx, size_full)
+                spectrum_i = spectrum_i ./ sum(spectrum_i)
+                spectrum, y = compute_spectrum_according_to_actual_spectrum2(spectrum_i, s, Θ, actual_lambda, unique_idx, size_full)
+                return spectrum, cor(vec(regenerate_signal(Θ, y)), s)
+            else
+                if verbose
+                    println("Algorithm stopped due to maximum number of λ's were tried without convergence to the specified `ρ_thres`")
+                end
+                spec = pool_frequencies2(y_act, length(s), unique_idx, size_full)
+                spec = spec ./ sum(spec)
+                return spec , ρ_act
+            end
+            break
+        elseif abs(ρ_act - ρ_thres) <= tol
+            spec = pool_frequencies2(y_act, length(s), unique_idx, size_full)
+            spec = spec ./ sum(spec)
+            return spec , ρ_act
+        end
+
+        # try new lambda
+        actual_lambda = lambda_min + (lambda_max - lambda_min)/2
+        # make the regression with specific lambda
+        path = glmnet(Θ, @view s[:]; lambda = [actual_lambda])
+        # check whether the regenerated signal matches with the given threshold
+        rr = cor(vec(regenerate_signal(Θ, path.betas)), s)
+
+        # pick the new bisection interval
+        if isnan(rr)
+            lambda_max = actual_lambda
+        elseif rr < ρ_thres
+            lambda_max = actual_lambda
+            y_act[:] = path.betas
+            ρ_act = rr
+        elseif rr > ρ_thres
+            lambda_min = actual_lambda
+            ρ_act = rr
+            y_act[:] = path.betas
+        end
+    end
+end
+
 """
     Determine the right regularization parameter with respect to ρ_thres & tol
     using the Newton-method.
@@ -122,6 +205,33 @@ function compute_spectrum_according_to_actual_spectrum(spectrum_i::Vector, s::Ve
     end
     return spectrum, y_act
 end
+function compute_spectrum_according_to_actual_spectrum2(spectrum_i::Vector, s::Vector,  Θ::SparseMatrixCSC, λ_max::Real, unique_idx::Vector, size_full::Integer)
+    abs_tol = 1e-6
+    max_iter = 10 # precision
+    λ_min = 0
+    y_act = zeros(size(Θ,2))
+    spectrum = zeros(size(spectrum_i))
+    # bisection search
+    for i = 1:max_iter
+        # try new lambda
+        actual_λ = λ_min + (λ_max - λ_min)/2
+        # make the regression with specific lambda
+        path = glmnet(Θ, @view s[:]; lambda = [actual_λ])
+        y_act[:] = path.betas
+        spectrum[:] = pool_frequencies2(y_act, length(s), unique_idx, size_full)
+        spectrum = spectrum ./ sum(spectrum)
+        # check whether the spectrum matches with the initial spectrum (input)
+        rr = cor(spectrum, spectrum_i)
+
+        # pick the new bisection interval
+        if rr > 1-abs_tol
+            λ_max = actual_λ
+        elseif rr < 1-abs_tol
+            λ_min = actual_λ
+        end
+    end
+    return spectrum, y_act
+end
 
 # regenerate a decomposed signal from basis functions and its coefficients
 function regenerate_signal(Θ::SparseMatrixCSC, coefs::CompressedPredictorMatrix)
@@ -132,6 +242,22 @@ end
 # pool the same frequencies
 function pool_frequencies(y::Vector, N::Integer)
     y = abs.(y)
+    spectrum = zeros(N)
+    cnt = 1
+    for i = 1:N
+        occs = sum(y[cnt:cnt+i-1] .> 0)
+        if occs>0
+            spectrum[i] = sum(y[cnt:cnt+i-1]) / occs
+        else
+            spectrum[i] = 0
+        end
+        cnt += i
+    end
+    return spectrum
+end
+function pool_frequencies2(yy::Vector, N::Integer, unique_idx::Vector, size_full::Integer)
+    y = zeros(size_full)
+    y[unique_idx] .= abs.(yy)
     spectrum = zeros(N)
     cnt = 1
     for i = 1:N
@@ -215,6 +341,43 @@ function create_single_basis_function(N::Int, period::Int)
         cs[j+1,:] = inp_array[1:N]
     end
     return cs
+end
+
+# Compute all line indices, which correspond to non-unique lines in the matrix,
+# which would store all possible `sum(1:N)` spike-basis functions.
+function non_unique_lines(N::Integer)
+    @assert N > 0
+    idx = Int[]
+    if N == 1 || N == 2
+        return idx
+    end
+    M = floor(N/2 +1)
+    LPD = N  # init lowest possible spike period
+    for shift = 1:N-2
+        # lowest possible spike period for this shift
+        if M-(N-shift) == 1
+            # this causal is needed for even and uneven N
+            if LPD-shift == 1
+                nothing
+            elseif LPD-shift == 0
+                LPD +=1
+            end
+        elseif N-shift >= M
+            LPD = N-shift
+        elseif N-shift < M
+            LPD += 1
+        end
+        for i = 1:(N-LPD)
+            push!(idx, index_in_basis_function_matrix(LPD+i)+shift)
+        end
+    end
+    return idx
+end
+
+# For an inter spike period T, compute the line index in the corresponding matrix,
+# which would store all possible `sum(1:N)` spike-basis functions.
+function index_in_basis_function_matrix(T::Integer)
+    return (T*((1+T)/2) - (T-1))
 end
 
 # regenerate a decomposed signal from basis functions and its coefficients
--- testing_iss_stuff.jl
+++ testing_iss_stuff.jl
@@ -14,10 +14,41 @@ using Random
 using GLMNet
 using Statistics
 using SparseArrays
+using BenchmarkTools
+using SparseArrays
+
+using DynamicalSystemsBase
+using InterSpikeSpectra
+using RecurrenceAnalysis
+using DelimitedFiles
+using DelayEmbeddings
+using Random
+
+using PyPlot
+pygui(true)
+
+## We illustrate the Inter Spike Spectrum for the paradigmatic Roessler system
 
 
-s = zeros(100)
-period1 = 3
-s[2:period1:end].= 1
+ds = Systems.lorenz()
+data = trajectory(ds,200)
+data1 = data[15001:end,:]
 
-spectrum, ρ = inter_spike_spectrum(s)
+figure()
+plot(data1[:,1])
+
+RP1 = RecurrenceAnalysis.RecurrenceMatrix(data1[1:5000,:], ε; fixedrate = true)
+τ_rr1 = RecurrenceAnalysis.tau_recurrence(RP1)
+τ_rr1 = τ_rr1[1:N] ./ maximum(τ_rr1[1:N])
+
+figure()
+plot(τ_rr1)
+
+threshold = 0.85
+tol = 1e-3
+
+M = 200
+@time spectrum, ρ = inter_spike_spectrum(τ_rr1[1:M]; ρ_thres = threshold, tol = tol)
+@time spectrum2, ρ2 = InterSpikeSpectra.inter_spike_spectrum2(τ_rr1[1:M]; ρ_thres = threshold, tol = tol)
+
+using BenchmarkTools
